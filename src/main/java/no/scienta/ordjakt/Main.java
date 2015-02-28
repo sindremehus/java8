b4ab14fabb954f3e3d19661c192585b47e2a54d2
Sindre Mehus <sm@scienta.no>
2015-02-28 16:08:44 +0100

    Improved Main.

--- src/main/java/no/scienta/ordjakt/Main.java
+++ src/main/java/no/scienta/ordjakt/Main.java
@@ -10,19 +10,44 @@ import java.io.InputStreamReader;
  */
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    private final Dictionary dictionary = new Dictionary();
 
-        System.out.print("Enter 16 characters: ");
+    public Main() throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String line = reader.readLine();
 
+        while (true) {
+            String line = readLine(reader);
+            if (line == null) {
+                System.out.println("Bye.");
+                System.exit(0);
+            }
+            solve(line);
+        }
+    }
+
+    private String readLine(BufferedReader reader) throws IOException {
+        String line = "";
+        while (line.length() != 16) {
+            System.out.print("Enter 16 characters: ");
+            line = reader.readLine().replaceAll("\\s+", "");
+            if (line.isEmpty()) {
+                return null;
+            }
+        }
+        return line;
+    }
+
+    private void solve(String line) {
         Board board = new BoardBuilder().build();
         board.setCharacters(line);
 
-        Dictionary dictionary = new Dictionary();
         Solver solver = new Solver(board, dictionary);
         solver.solve();
 
         System.out.println(solver.getSolutions());
+    }
+
+    public static void main(String[] args) throws IOException {
+        new Main();
     }
 }
